b23a10a3997d055585b327bd538a2dcd92526e9b
Yadier01 <yadierg7@gmail.com>
2024-03-14 01:56:42 -0400

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
 console.log("Hello via Bun!");
+import express from "express"
+
+const app = express()
+const obj = {
+  name: "Jhon",
+  lastname: "doe",
+  age: 24
+
+}
+
+app.get("/", (req, res) => {
+  res.status(200).json({ data: obj })
+})
+app.listen("3000", () => {
+  console.log("listen 3001")
+});
+
